83cb0dbdcc8318307ce8314180facd10f69a5df2
Mayra <mayra.cademartori@gmail.com>
2021-04-01 00:08:20 -0300

    Delete(js files)

--- src/index.spec.js
+++
@@ -1,8 +0,0 @@
-import { BotInit } from './index.js'
-
-describe('Index', () => {
-    const BotInitMock = jest.fn(BotInit)
-    it('should BotInit to be defined', () => {
-        expect(BotInitMock).toBeDefined();
-    })
-})
